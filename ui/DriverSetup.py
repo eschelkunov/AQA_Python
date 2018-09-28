8d081f95a4d22edd468f0b70145b66b59cf4f054
yshchelkunov <Yevhen.Shchelkunov@sizmek.com>
2018-09-29 01:52:40 +0300

    added/configured webdriver manager for linux

--- ui/DriverSetup.py
+++ ui/DriverSetup.py
@@ -2,6 +2,7 @@ import platform
 import os
 import pytest
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 class DriverSetup:
@@ -12,21 +13,15 @@ class DriverSetup:
         options.add_argument('--ignore-certificate-errors')
         options.add_argument('--ignore-ssl-errors')
         options.add_argument('start-maximized')
-        dir_path = os.path.dirname(os.path.realpath(__file__))
         if platform.system() == "Darwin":
+            dir_path = os.path.dirname(os.path.realpath(__file__))
             chromedriver = dir_path + "/webdrivers/chromedriver"
             os.environ["webdriver.chrome.driver"] = chromedriver
             self.driver = webdriver.Chrome(options=options, executable_path=chromedriver)
         else:
-            chromedriver = dir_path + "/webdrivers/chromedriver_linux"
-            os.environ["webdriver.chrome.driver"] = chromedriver
-            self.driver = webdriver.Chrome(options=options, executable_path=chromedriver)
+            self.driver = webdriver.Chrome(options=options, executable_path=ChromeDriverManager().install())
         self.driver.implicitly_wait(10)
-        # self.driver.maximize_window()
-
-
-        yield  # everything after 'yield' is executed on tear-down
-
+        yield
         if (self.driver != None):
             self.driver.close()
             self.driver.quit()
